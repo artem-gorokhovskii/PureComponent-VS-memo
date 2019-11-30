7e12a0a5daa90da0d8e00c4e701774eee7568075
Artem Gorokhovskii <a84277755@gmail.com>
2019-12-01 00:18:23 +0300

    implemented React.memo in Parent component

--- src/Parent.jsx
+++ src/Parent.jsx
@@ -1,40 +1,64 @@
 import React from "react";
 import { App } from "./App.jsx";
 
-export class Parent extends React.Component {
-    constructor(props){
-        super(props);
-        console.log("=== Первичное монтирование начато ===");
-    }
+// export class Parent extends React.Component {
+//     constructor(props){
+//         super(props);
+//         console.log("=== Первичное монтирование начато ===");
+//     }
 
-    componentDidMount() {
+//     componentDidMount() {
         
-        console.log("=== Первичное монтирование завершено ===");
-    }
+//         console.log("=== Первичное монтирование завершено ===");
+//     }
 
-    state = {
-        singleName: "Одинокий Джо",
-        manyNames: ["Игорь", "Илья"],
-        uselessValue: 1
+//     state = {
+//         singleName: "Одинокий Джо",
+//         manyNames: ["Игорь", "Илья"],
+//         uselessValue: 1
+//     };
+
+//   changeUselessValue = () => {
+//         console.log("=== Вызываем Render у Parent (обновили state) ===")
+//         this.setState({ uselessValue: Math.random() });
+//   };
+
+//   render() {
+//     const { singleName, manyNames, uselessValue } = this.state;
+//     console.log("Render Parent (Component)");
+//     return (
+//         <div>
+//             <App
+//                 singleName={singleName}
+//                 manyNames={manyNames}
+//                 // uselessValue={uselessValue}
+//                 changeUselessValue={this.changeUselessValue}
+//             />
+//       </div>
+//     );
+//   }
+// }
+
+export const Parent = React.memo(props => {
+    const [singleName] = React.useState("Одинокий Джо");
+    const [manyNames] = React.useState(() => ["Игорь", "Илья"]);
+    const [uselessValue, setUselessValue] = React.useState(1);    
+
+    console.log("Render Parent (React.memo)");
+
+    const changeUselessValue = () => {
+        console.log("=== Вызываем Render у Parent (обновили state) ===")
+        setUselessValue(Math.random());
     };
 
-  changeUselessValue = () => {
-        console.log("=== Вызываем Render у Parent (обновили state) ===")
-        this.setState({ uselessValue: Math.random() });
-  };
-
-  render() {
-    const { singleName, manyNames, uselessValue } = this.state;
-    console.log("Render Parent (Component)");
     return (
         <div>
             <App
                 singleName={singleName}
                 manyNames={manyNames}
                 // uselessValue={uselessValue}
-                changeUselessValue={this.changeUselessValue}
+                changeUselessValue={changeUselessValue}
             />
       </div>
     );
-  }
-}
+});
